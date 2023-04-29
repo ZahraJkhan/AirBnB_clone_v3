d1e8973d547388616e9f0f811cbb4b9d1c906ee0
ZahraJkhan <zahrajid@gmail.com>
2023-04-29 11:40:32 -0700

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,57 +1,35 @@
+#!/usr/bin/python3
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 errors, in the event that global error handler fails
-    """
-    code = exception.__str__().split()[0]
-    description = exception.description
-    message = {'error': description}
-    return make_response(jsonify(message), code)
-
-
-@app.errorhandler(400)
-def handle_404(exception):
-    """
-    handles 400 errros, in the event that global error handler fails
-    """
-    code = exception.__str__().split()[0]
-    description = exception.description
-    message = {'error': description}
-    return make_response(jsonify(message), code)
-
-
-@app.errorhandler(Exception)
-def global_error_handler(err):
-    """
-        Global Route to handle All Error Status Codes
-    """
-    if isinstance(err, HTTPException):
-        if type(err).__name__ == 'NotFound':
-            err.description = "Not found"
-        message = {'error': err.description}
-        code = err.code
-    else:
-        message = {'error': err}
-        code = 500
-    return make_response(jsonify(message), code)
-
-
-def setup_global_errors():
-    """
-    This updates HTTPException Class with custom error function
-    """
-    for cls in HTTPException.__subclasses__():
-        app.register_error_handler(cls, global_error_handler)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App
-    """
-    # initializes global error handling
-    setup_global_errors()
-    # start Flask app
-    app.run(host=host, port=port))
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
